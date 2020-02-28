57498152445f08d764c95cc7961679a72db6841f
Stone <18693931625@163.com>
2020-02-28 15:10:35 +0300

    first time

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String []args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter the number of integers:");
+        int num = scan.nextInt();
+        int[] Ary = new int[num];
+        System.out.println("Please enter the number:");
+        int sum = 0;
+        int pro = 1;
+        for (int i = 0; i < Ary.length; i++){
+            Ary[i] = scan.nextInt();
+            sum += Ary[i];
+            pro *= Ary[i];
+        }
+        System.out.println("Sum="+sum);
+        System.out.println("pro="+pro);
+
+
+
+    }
+
+}
