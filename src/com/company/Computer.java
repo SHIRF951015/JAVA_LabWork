a479273b7c9c893e1e1d9d13764004b9d630f538
Stone <18693931625@163.com>
2020-05-30 16:07:01 +0300

    first time

---
+++ src/com/company/Computer.java
@@ -0,0 +1,63 @@
+/*
+使用内部类创建一个类Computer（计算机），使用该类的对象可以存储有关操作系统，处理器和RAM的信息。
+ */
+package com.company;
+import java.util.*;
+
+public class Computer {
+    public static void main(String[] args) {
+        Computer com = new Computer();
+        com.info.print();
+
+    }
+    public static Scanner scan = new Scanner(System.in);
+    public String computer;
+    public info info;
+    public Computer(){
+        System.out.println("Enter the computer name:");
+        setCom(scan.nextLine());
+        info = new info();
+    }
+    public String getCom(){
+        return computer;
+    }
+    public void setCom(String computer){
+        this.computer = computer;
+    }
+    public class info{
+        String sys,cpu,ram;
+        public info(){
+            System.out.println("Enter system:");
+            setSys(scan.nextLine());
+            System.out.println("Enter cpu:");
+            setCpu(scan.nextLine());
+            System.out.println("Enter ram:");
+            setRam(scan.nextLine());
+        }
+        public String getSys(){
+            return sys;
+        }
+        public String getCpu(){
+            return cpu;
+        }
+        public String getRam(){
+            return ram;
+        }
+        public void setSys(String sys){
+            this.sys = sys;
+        }
+        public void setCpu(String cpu){
+            this.cpu = cpu;
+
+        }
+        public void setRam(String ram){
+            this.ram = ram;
+        }
+        public void print(){
+            System.out.println("Computer "+getCom()+":"+getSys()+" system,"+getCpu()+" cpu,"+getRam()+" ram.");
+        }
+    }
+
+}
+
+
